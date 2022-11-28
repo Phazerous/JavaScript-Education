e5d5a973c8b0053b016f3d5b1222c8f28614c860
Phazerous <nphazerous@gmail.com>
2022-11-29 00:53:24 +0600

    Https module

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-const button = document.querySelector('#mainButton');
+const https = require('https');
 
-button.addEventListener('click', () => {
-    button.innerHTML = 'Welcome to asynchronous programming!';
-})
+function getJSON(url, callback) {
+    const request = https.get(url);
+    
+    request.on('response', response => {
+        let httpStatus = response.statusCode;
+        response.setEncoding('utf-8');
+        let body = "";
+
+        response.on('data', (chunk) => { body += chunk});
+
+        response.on('end', () => {
+            if (httpStatus === 200) {
+                callback(null, body);
+            } else {
+                callback(httpStatus, null);
+            }
+        });
+
+        response.on('error', () => {
+            callback(err, null);
+        });
+    })
+}
+
+function callback(httpStatus, body) {
+    if (httpStatus !== null) {
+        console.log('Error with ' + httpStatus);
+    } else {
+        console.log(JSON.parse(body));
+    }
+}
+
+getJSON('https://api.publicapis.org/random', callback);
