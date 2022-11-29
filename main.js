02a316e244d5bf58d293514885e337a823e48e85
Phazerous <nphazerous@gmail.com>
2022-11-30 01:31:32 +0600

    getJSON Promise

--- main.js
+++ main.js
@@ -1,86 +1,40 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var DataTable = /** @class */ (function () {
-    function DataTable() {
-    }
-    DataTable.prototype.open = function () {
-        this.table = this.createTable();
-        this.table.open();
-    };
-    DataTable.prototype.read = function () {
-        console.log(this.table.read());
-    };
-    DataTable.prototype.close = function () {
-        this.table.close();
-    };
-    return DataTable;
-}());
-var ExcelCreator = /** @class */ (function (_super) {
-    __extends(ExcelCreator, _super);
-    function ExcelCreator() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    ExcelCreator.prototype.createTable = function () {
-        return new Excel();
-    };
-    return ExcelCreator;
-}(DataTable));
-var GoogleSheetsCreator = /** @class */ (function (_super) {
-    __extends(GoogleSheetsCreator, _super);
-    function GoogleSheetsCreator() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    GoogleSheetsCreator.prototype.createTable = function () {
-        return new GoogleShets();
-    };
-    return GoogleSheetsCreator;
-}(DataTable));
-var Excel = /** @class */ (function () {
-    function Excel() {
-    }
-    Excel.prototype.open = function () {
-        console.log('Excel opened!');
-    };
-    Excel.prototype.read = function () {
-        return 'Reading data from Excel...';
-    };
-    Excel.prototype.close = function () {
-        console.log('Excel collapsed.');
-    };
-    return Excel;
-}());
-var GoogleShets = /** @class */ (function () {
-    function GoogleShets() {
-    }
-    GoogleShets.prototype.open = function () {
-        console.log('Google Sheets... open new opportunities.');
-    };
-    GoogleShets.prototype.read = function () {
-        return "[row1, column1], [row2, column1], ...";
-    };
-    GoogleShets.prototype.close = function () {
-        console.log('See you soon!');
-    };
-    return GoogleShets;
-}());
-var creator = new GoogleSheetsCreator();
-creator.open();
-creator.read();
-creator.close();
-creator = new ExcelCreator();
-creator.open();
-creator.read();
-creator.close();
+const http = require('http');
+
+function getJSON(url) {
+    return new Promise((resolve, reject) => {
+        let request = http.get(url, response => {
+            if (response.statusCode !== 200) {
+                reject(new Error('Bad status code.'))
+            } else if (!response.headers["content-type"].includes("application/json")) {
+                console.log(response.headers["content-type"]);
+                reject(new Error('Wrong content type!'))
+            } else {
+                let body = "";
+                response.setEncoding('UTF-8');
+    
+                response.on('data', (chunk) => {body += chunk});
+    
+                response.on('end', () => {
+                    try {
+                        let parse = JSON.parse(body);
+
+                        resolve(parse)
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            }
+
+            request.on('error', error => {
+                reject(error);
+            })
+        });
+    });
+}
+
+getJSON('http://apiv3.iucnredlist.org/api/v3/version')
+    .then(res => {
+        console.log(res);
+    }).catch(err => {
+        console.log(err.message);
+    })
