20e90a81f1b0968494777b3044824a1c1420a50c
Phazerous <nphazerous@gmail.com>
2022-12-12 00:32:16 +0600

    DeepPick Recursion

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-const users = {
-  u1: 'Phazerous',
-  u2: 'Phazzy',
-  u3: 'Dangerous'
+const dan = {
+  type: "person",
+  data: {
+    gender: "male",
+    info: {
+      id: 22,
+      fullname: {
+        first: "Dan",
+        last: "Deacon"
+      }
+    }
+  }
+};
+
+const deepPick = function (fieldDotNotation, obj = {}) {
+  const [first, ...remaining] = fieldDotNotation.split('.');
+  return remaining.length ? deepPick(remaining.join('.'), obj[first]) : obj[first];
 }
 
-const updatedUsers = Object.keys(users).map(key => ({ id: key, nickname: users[key] }));
-console.log(updatedUsers);
+console.log(deepPick("type", dan)); // "person"
+console.log(deepPick("data.info.fullname.first", dan)); // "Dan"
