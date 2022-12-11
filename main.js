11e9021e958e9d836d4c389630140b4471f22958
Phazerous <nphazerous@gmail.com>
2022-12-11 22:43:51 +0600

    Restructuration | Extending object literal

--- main.js
+++ main.js
@@ -1,40 +1,6 @@
-const http = require('http');
+const name = 'Phazerous';
+const callSign = 'Dangerous';
 
-function getJSON(url) {
-    return new Promise((resolve, reject) => {
-        let request = http.get(url, response => {
-            if (response.statusCode !== 200) {
-                reject(new Error('Bad status code.'))
-            } else if (!response.headers["content-type"].includes("application/json")) {
-                console.log(response.headers["content-type"]);
-                reject(new Error('Wrong content type!'))
-            } else {
-                let body = "";
-                response.setEncoding('UTF-8');
-    
-                response.on('data', (chunk) => {body += chunk});
-    
-                response.on('end', () => {
-                    try {
-                        let parse = JSON.parse(body);
+const user = { name, callSign }
 
-                        resolve(parse)
-                    } catch (e) {
-                        reject(e);
-                    }
-                });
-            }
-
-            request.on('error', error => {
-                reject(error);
-            })
-        });
-    });
-}
-
-getJSON('http://apiv3.iucnredlist.org/api/v3/version')
-    .then(res => {
-        console.log(res);
-    }).catch(err => {
-        console.log(err.message);
-    })
+console.log(user);
