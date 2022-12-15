d226656f8ddcf024bf3e98addb27f8beb0f30348
Phazerous <nphazerous@gmail.com>
2022-12-16 03:14:37 +0600

    sqlite3 | Inserting data in db

--- main.js
+++ main.js
@@ -1,40 +1,18 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('./db/chinook.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('./db/sample.db', sqlite3.OPEN_READWRITE, (err) => console.log(err));
+
+const languages = ['Java', 'JavaScript', 'Python', 'C#', 'Go', 'C', 'C++', 'HTML', 'CSS'];
+const placeholders = languages.map((language) => '(?)').join(', ');
+const sqlQuery = `INSERT INTO langs(name) VALUES ` + placeholders;
+
+db.run(sqlQuery, languages, function (err) {
   if (err) {
-    console.log(err.message);
+    console.log(err);
   }
 
-  console.log('Connected to the words.db');
-})
-
-const sqlQuery = `SELECT AlbumId id, Title title from albums WHERE AlbumId = ?`;
-const albumId = 1;
-
-// const getAllQuery = `SELECT * FROM albums`;
-// db.all(getAllQuery, [], (err, rows) => {
-//   if (err) {
-//     console.log(err.message);
-//   }
-
-//   rows.forEach((row) => {
-//     console.log(row.AlbumId, row.Title, row.ArtistId)
-//   })
-// });
-
-
-db.get(sqlQuery, [albumId], (err, row) => {
-  if (err) {
-    console.log(err.message);
-  }
-
-  console.log(row.id, row.title)
-})
-
-db.close((err) => {
-  if (err) {
-    console.log(err.message);
-  }
-
-  console.log('Close the words.db connection');
+  console.log(`Added with id of ${this.lastID}`);
+  console.log(this.changes);
 });
+
+db.close();
