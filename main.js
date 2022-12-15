e5ebb8253ea3f6e675238828e082bdbb7defb021
Phazerous <nphazerous@gmail.com>
2022-12-16 02:42:46 +0600

    sqlite3 | Open and Close Connection

--- main.js
+++ main.js
@@ -1,60 +1,17 @@
-const oneSecond = () => 1000;
-const getCurrentTime = () => new Date();
-const clear = () => console.clear();
-const log = message => console.log(message);
+const sqlite3 = require('sqlite3').verbose();
 
-const getTimeClock = date => ({
-  hours: date.getHours(),
-  minutes: date.getMinutes(),
-  seconds: date.getSeconds()
+let db = new sqlite3.Database('./db/words.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.log(err.message);
+  }
+
+  console.log('Connected to the words.db');
+})
+
+db.close((err) => {
+  if (err) {
+    console.log(err.message);
+  }
+
+  console.log('Close the words.db connection');
 });
-
-const appendAMPM = timeClock => ({
-  ...timeClock,
-  ampm: (timeClock.hours >= 12) ? "PM" : "AM"
-});
-
-const civilianHours = timeClock => ({
-  ...timeClock,
-  hours: (timeClock.hours >= 12) ? timeClock.hours - 12 : timeClock.hours
-});
-
-const getCivilianTime = timeClock => compose(
-  appendAMPM,
-  civilianHours
-)(timeClock);
-
-const prependZero = key => timeClock => ({
-  ...timeClock,
-  [key]: (timeClock[key] < 10) ? "0" + timeClock[key] : timeClock[key]
-});
-
-const doubleDigits = timeClock => compose(
-  prependZero("hours"),
-  prependZero("minutes"),
-  prependZero("seconds")
-)(timeClock);
-
-const formatClock = format => time => format
-  .replace("hh", time.hours)
-  .replace("mm", time.minutes)
-  .replace("ss", time.seconds)
-  .replace("tt", time.ampm)
-
-const compose = (...fns) => arg => fns.reduce((composed, fn) => fn(composed), arg);
-
-const display = func => arg => func(arg);
-
-const startTicking = () => setInterval(
-  compose(
-    clear,
-    getCurrentTime,
-    getTimeClock,
-    getCivilianTime,
-    doubleDigits,
-    formatClock("hh:mm:ss tt"),
-    display(log)
-  ), oneSecond()
-);
-
-startTicking();
