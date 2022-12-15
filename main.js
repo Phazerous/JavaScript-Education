7248d1bfb4d4e4ff5b3f2980ce5348bbe242f4da
Phazerous <nphazerous@gmail.com>
2022-12-16 03:01:20 +0600

    sqlite3 | Getting tuples

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('./db/words.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('./db/chinook.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.log(err.message);
   }
 
   console.log('Connected to the words.db');
+})
+
+const sqlQuery = `SELECT AlbumId id, Title title from albums WHERE AlbumId = ?`;
+const albumId = 1;
+
+// const getAllQuery = `SELECT * FROM albums`;
+// db.all(getAllQuery, [], (err, rows) => {
+//   if (err) {
+//     console.log(err.message);
+//   }
+
+//   rows.forEach((row) => {
+//     console.log(row.AlbumId, row.Title, row.ArtistId)
+//   })
+// });
+
+
+db.get(sqlQuery, [albumId], (err, row) => {
+  if (err) {
+    console.log(err.message);
+  }
+
+  console.log(row.id, row.title)
 })
 
 db.close((err) => {
